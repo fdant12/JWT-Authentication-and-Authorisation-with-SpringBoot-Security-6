bd1edcc929b9b0d848819b4e5c33c80aec3f38f3
fdan12 <fguirol@gmail.com>
2025-01-28 00:24:25 +0100

    Configure the http security with securityFilterChain + configure the authentication provider

--- src/main/java/com/example/jwtauthenticationandauthorisationwithspringbootsecurity6/configuration/ApplicationConfig.java
+++ src/main/java/com/example/jwtauthenticationandauthorisationwithspringbootsecurity6/configuration/ApplicationConfig.java
@@ -2,12 +2,14 @@ package com.example.jwtauthenticationandauthorisationwithspringbootsecurity6.con
 
 import com.example.jwtauthenticationandauthorisationwithspringbootsecurity6.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,5 +21,19 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService () {
       return username -> repository.findByEmail(username)
               .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-    };
+    }
+
+    // DAO -> Responsible to fetch the userDetails and also encode password
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
